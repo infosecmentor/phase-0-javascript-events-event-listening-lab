73a0f268291c49b92486f720dcd26cd1877515a1
infosecmentor <muhambiphares@gmail.com>
2022-06-02 20:42:37 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-function addingEventListener() {
+const clickAlert = () => {
+    alert('Trudy Judy');
 }
+
+function addingEventListener() {
+    const input = document.getElementById('input');
+    input.addEventListener('click', clickAlert);
+}
+
+addingEventListener()
